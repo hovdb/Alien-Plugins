f26b019f11a64522c4b80ce748e79b0a3d978f9d
Bjorn <bhovd@FJORD.local>
2015-01-30 16:37:50 -0600

    updated avaya plugin

--- avaya.sql
+++ avaya.sql
@@ -1,12 +1,12 @@
--- Nortel Avaya
+-- Nortel Avaya 4548gt & 4850GTS
 -- plugin_id: 9225
 
 
 DELETE FROM plugin WHERE id = "9225"; 
 DELETE FROM plugin_sid where plugin_id = "9225";
-DELETE FROM software_cpe where plugin = 'nortel_avaya:9225';
+DELETE FROM software_cpe where plugin = 'avaya:9225';
 
-INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9225, 1, 'nortel_avaya', 'nortel_avaya', '24', 'nortel_avaya');
+INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9225, 1, 'avaya', 'avaya', '24', 'avaya');
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9225, 1, NULL, NULL, 'Connection closed (inactivity logout), IP address', 2, 2);
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9225, 2, NULL, NULL, 'Connection closed (user logout), IP address', 2, 2);
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9225, 3, NULL, NULL, 'Session closed (inactivity logout), IP address', 2, 2);
@@ -21,4 +21,4 @@ INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, prio
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9225, 12, NULL, NULL, 'S ssh', 2, 2);
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9225, 2000000000, NULL, NULL, 'generic', 2, 2);
 
-INSERT IGNORE INTO software_cpe (cpe, name, version, line, vendor, plugin) VALUES ('cpe:/o:nortel_avaya:nortel_avaya:nortel_avaya', 'nortel_avaya','nortel_avaya','nortel_avaya','nortel_avaya', 'nortel_avaya:9225');
+INSERT IGNORE INTO software_cpe (cpe, name, version, line, vendor, plugin) VALUES ('cpe:/o:avaya:4548gt_switch:4548gt', 'avaya','4548gt','avaya 4548gt','avaya', 'avaya:9225');
