f0c4db6cd3c2d8c4362ed400adaeb23077ac461d
Bjorn <bhovd@FJORD.local>
2015-04-11 11:28:37 -0500

    updated
    made chanes to vb script and firewall files

--- mcafee_firewall_enterprise.sql
+++ mcafee_firewall_enterprise.sql
@@ -6,7 +6,7 @@ DELETE FROM plugin WHERE id = '9230';
 DELETE FROM plugin_sid where plugin_id = '9230';
 DELETE FROM software_cpe where plugin = 'mcafee_firewall_enterprise:9230';
 INSERT IGNORE INTO software_cpe (cpe, name, version, line, vendor, plugin) VALUES ('cpe:/o:McAfee:Firewall Enterprise:','Firewall Enterprise','','','McAfee', 'mcafee_firewall_enterprise:9230');
-INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9230, 1, 'mcafee_firewall_enterprise', 'mcafee_firewall_enterprise', '24', 'McAfee');
+INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9230, 1, 'mcafee_firewall_enterprise', 'mcafee_firewall_enterprise', '10', 'McAfee');
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9230, 1, NULL, NULL, 'ACLD', 2, 2);
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9230, 2, NULL, NULL, 'DNS Proxy', 2, 2);
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9230, 3, NULL, NULL, 'HTTP Proxy', 2, 2);
