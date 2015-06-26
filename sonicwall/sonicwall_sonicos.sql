50193c453ab26731f8cdb1e2167b602fdb443b8d
Bjorn <bhovd@FJORD-2.local>
2015-06-26 14:41:40 -0500

    wrong sid

--- sonicwall/sonicwall_sonicos.sql
+++ sonicwall/sonicwall_sonicos.sql
@@ -4,7 +4,7 @@ DELETE FROM software_cpe where plugin = 'sonicwall_sonicos:9300';
 INSERT IGNORE INTO software_cpe (cpe, name, version, line, vendor, plugin) VALUES ('cpe:/o:SonicWall:SonicOS:5.9.1.0-22o','SonicOS','5.9.1.0-22o','','SonicWall','sonicwall_sonicos:9300');
 INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES ('9300',1,'sonicwall_sonicos','sonicwall_sonicos','10','SonicWall');
 INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1154, 3, 1, 'Application Control Detection Alert');
-INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1154, 3, 1, 'Application Control Prevention Alert');
+INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1155, 3, 1, 'Application Control Prevention Alert');
 INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1226, 3, 1, 'HTTPS Handshake');
 INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1232, 3, 1, 'NTP Request sent');
 INSERT INTO `plugin_sid` (`plugin_id`,`sid`,`reliability`, `priority`, `name`) VALUES (9300, 1233, 3, 1, 'Unhandled link-local or multicast IPv6 packet dropped');
